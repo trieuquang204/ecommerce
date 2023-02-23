88a709d34b390e98c4144a9df57eead818df92c6
trieuquang204 <106046496+trieuquang2106@users.noreply.github.com>
2023-02-23 21:47:16 +0700

    Hash password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require('bcrypt')
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -58,5 +59,9 @@ var userSchema = new mongoose.Schema({
   timestamps: true
 });
 
+userSchema.pre('save', async function(next) {
+  const salt = bcrypt.genSaltSync(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 //Export the model
 module.exports = mongoose.model('User', userSchema);
