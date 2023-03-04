4a7c89e6b9b1c4e2b45ad56c144186bb2fa1e7be
trieuquang204 <106046496+trieuquang2106@users.noreply.github.com>
2023-03-04 22:27:42 +0700

    Access token và refresh token

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
 const {generateAccessToken, generateRefreshAccessToken} = require('../middlewares/jwt')
+const jwt = require('jsonwebtoken')
 
 
 const register = asyncHandler(async(req, res) => {
@@ -69,8 +70,53 @@ const getCurrent = asyncHandler(async(req, res) => {
   })
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  // Lay token tu cookie
+  const cookie = req.cookies
+  if(!cookie && !cookie.refreshToken) {
+    throw new Error('No refresh token in cookies')
+  }
+  const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET)
+  const response = await User.findOne({_id: rs._id, refreshToken: cookie.refreshToken})
+  return res.status(200).json({
+    success: response ? true : false,
+    newAccessToken: response ? generateAccessToken(response._id, response.role) : 'Refresh token not match!'
+  })
+
+  // jwt.verify(cookie.refreshToken, process.env.JWT_SECRET, async (err, decode) => {
+  //   if(err) throw new Error('Invalid refresh token')
+  //   // Check token co giong vs token da luu trong db k
+
+  //   const response = await User.findOne({_id: decode._id, refreshToken: cookie.refreshToken})
+  //   return res.status(200).json({
+  //     success: response ? true : false,
+  //     newAccessToken: response ? generateAccessToken(response._id, response.role) : 'Refresh token not match!'
+  //   })
+  // })
+})
+
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies
+  if(!cookie || !cookie.refreshToken) throw new Error('No refresh token in cookies')
+
+  // Xoa refreshToken trong DB
+  await User.findOneAndUpdate({refreshToken: cookie.refreshToken}, { refreshToken: ''}, { new: true })
+  // Xoa refreshToken o cookie trinh duyet
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    secure: true
+  })
+  return res.status(200).json({
+    success: true,
+    mes: 'Logout successfully!'
+  })
+})
+
+
 module.exports = {
   register,
   login,
-  getCurrent
+  getCurrent,
+  refreshAccessToken,
+  logout
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,5 +5,7 @@ const { verifyAccessToken} = require('../middlewares/verifyToken')
 router.post('/register', ctrls.register)
 router.post('/login', ctrls.login)
 router.get('/current', verifyAccessToken, ctrls.getCurrent)
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
 
 module.exports = router
