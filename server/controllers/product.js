48da9a39e9c7fcc6e780223ad4151f4caa614a67
trieuquang204 <106046496+trieuquang2106@users.noreply.github.com>
2023-03-14 22:09:32 +0700

    [#9] CRUD product | E-Commerce Project | MERN

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -16,6 +16,54 @@ const createProduct = asyncHandler( async (req, res) => {
   })
 })
 
+const getProduct = asyncHandler( async (req, res) => {
+  const { pid } = req.params
+  const product = await Product.findById(pid)
+  return res.status(200).json({
+    success: product ? true : false,
+    productData: product ? product : 'Cannot get product!'
+  })
+})
+
+
+// Filtering, sorting, pagination
+const getProducts = asyncHandler( async (req, res) => {
+  const products = await Product.find()
+  return res.status(200).json({
+    success: products ? true : false,
+    productDatas: products ? products : 'Cannot get products!'
+  })
+})
+
+
+const updateProduct = asyncHandler( async (req, res) => {
+  const { pid } = req.params
+  if(req.body && req.body.title ) req.body.slug = slugify(req.body.title)
+
+  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {new: true })
+  return res.status(200).json({
+    success: updatedProduct ? true : false,
+    updatedProduct: updatedProduct ? updatedProduct : 'Cannot update product!'
+  })
+})
+
+const deleteProduct = asyncHandler( async (req, res) => {
+  const { pid } = req.params
+
+  const deletedProduct = await Product.findByIdAndDelete(pid)
+  return res.status(200).json({
+    success: deletedProduct ? true : false,
+    deletedProduct: deletedProduct ? deletedProduct : 'Cannot delete product!'
+  })
+})
+
+
+
+
 module.exports = {
-  createProduct
+  createProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct
 }
